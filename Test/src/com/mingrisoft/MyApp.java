ae2aba4f4f0ed8f263d70582913f09a7b675e671
Yaojiameng <1497691078@qq.com>
2016-10-17 22:27:43 +0800

    delete the compute of time

--- Test/src/com/mingrisoft/MyApp.java
+++ Test/src/com/mingrisoft/MyApp.java
@@ -387,8 +387,8 @@ public class MyApp {
 		Scanner s=new Scanner(System.in);
 		String str;
     	str=s.nextLine();//以字符串的形式读入多项式
-    	double time1 = System.currentTimeMillis();
-    	System.out.println("处理字符串开始的时间为:"+time1/60+"秒");
+//    	double time1 = System.currentTimeMillis();
+//    	System.out.println("处理字符串开始的时间为:"+time1/60+"秒");
     	String str_o="";
     	int f=0;
     	for(int i=0;i<str.length();i++)//处理空格和制表符
@@ -412,10 +412,10 @@ public class MyApp {
 		}
 		else
 			System.out.println("您输入的表达式不合法！");
-		double time2 = System.currentTimeMillis();
-    	System.out.println("处理字符串结束的时间为:"+time2/60+"秒");
-    	double time3 = time2-time1;
-    	System.out.println("处理字符串的总时间为:"+time3/60+"秒");
+//		double time2 = System.currentTimeMillis();
+//    	System.out.println("处理字符串结束的时间为:"+time2/60+"秒");
+//    	double time3 = time2-time1;
+//    	System.out.println("处理字符串的总时间为:"+time3/60+"秒");
 		
 		String order1="!simplify ";
 		String order2="!d/d";
@@ -425,8 +425,8 @@ public class MyApp {
 		{
 			if(order.charAt(1)=='d')
 			{
-				double time4 = System.currentTimeMillis();
-		    	System.out.println("开始执行指令的时间为:"+time4/60+"秒");
+//				double time4 = System.currentTimeMillis();
+//		    	System.out.println("开始执行指令的时间为:"+time4/60+"秒");
 				if(order.length()<=4)
 				{
 					System.out.println("非法指令！");
@@ -462,13 +462,13 @@ public class MyApp {
 							{
 								if(!a.derivative(bian))
 						    		System.out.println("非法的输入！");
-								else
-								{
-									double time5 = System.currentTimeMillis();
-									System.out.println("指令完成结束的时间为:"+time5/60+"秒");
-									double time6 = time5-time4;
-									System.out.println("指令执行的总时间为:"+time6/60+"秒");
-								}
+//								else
+//								{
+////									double time5 = System.currentTimeMillis();
+////									System.out.println("指令完成结束的时间为:"+time5/60+"秒");
+////									double time6 = time5-time4;
+////									System.out.println("指令执行的总时间为:"+time6/60+"秒");
+//								}
 							}
 							else
 								System.out.println("不存在此变量！");
@@ -479,13 +479,13 @@ public class MyApp {
 							{
 								if(!a.derivative(bian))
 						    		System.out.println("非法的输入！");
-								else
-								{
-									double time5 = System.currentTimeMillis();
-									System.out.println("指令完成结束的时间为:"+time5/60+"秒");
-									double time6 = time5-time4;
-									System.out.println("指令执行的总时间为:"+time6/60+"秒");
-								}
+//								else
+//								{
+//									double time5 = System.currentTimeMillis();
+//									System.out.println("指令完成结束的时间为:"+time5/60+"秒");
+//									double time6 = time5-time4;
+//									System.out.println("指令执行的总时间为:"+time6/60+"秒");
+//								}
 							}
 							else
 								System.out.println("不存在此变量！");
@@ -496,8 +496,8 @@ public class MyApp {
 			}
 			else if(order.charAt(1)=='s')
 			{
-				double time4 = System.currentTimeMillis();
-		    	System.out.println("开始执行指令的时间为:"+time4/60+"秒");
+//				double time4 = System.currentTimeMillis();
+//		    	System.out.println("开始执行指令的时间为:"+time4/60+"秒");
 				if(order.length()<9)
 				{
 					System.out.println("非法指令！");
@@ -607,10 +607,10 @@ public class MyApp {
 								if(flagg==0)
 								{
 									System.out.println(fanhui);
-									double time5 = System.currentTimeMillis();
-									System.out.println("指令完成结束的时间为:"+time5/60+"秒");
-									double time6 = time5-time4;
-									System.out.println("指令执行的总时间为:"+time6/60+"秒");
+//									double time5 = System.currentTimeMillis();
+//									System.out.println("指令完成结束的时间为:"+time5/60+"秒");
+//									double time6 = time5-time4;
+//									System.out.println("指令执行的总时间为:"+time6/60+"秒");
 								}
 								
 							}
